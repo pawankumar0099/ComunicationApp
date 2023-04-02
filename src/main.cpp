572dbe5868a62ec44a227fe03a32a6d48d9fef17
pawan <pawan@nagarro.com>
2023-04-02 11:10:15 +0530

    added IP string valiation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@
 #include <string>
 #include <stdexcept>
 
+bool is_valid_ip(const char* ip_str)
+{
+    // Regular expression pattern for a valid IP address
+    std::regex pattern("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$");
+
+    // Check if the string matches the pattern
+    return std::regex_match(ip_str, pattern);
+}
 
 
 int main(int argc, char *argv[])
@@ -17,29 +25,45 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    try
+    if (strcmp(argv[1], "network")==0)
     {
-        int com_type = Com_Type::NETWORK;
-        char *server_or_client = argv[2];
-        char *ip_address = argv[3];
-        int network_port = std::stoi(argv[4]);
-        unsigned short us_network_port = static_cast<unsigned short>(network_port);
 
-        Communication *communication = CommunicationObjectFactory::getObject(com_type, server_or_client, ip_address, us_network_port);
-        communication->handleMessage();
+        if (argc < 5)
+            std::cerr << "Invalid number of arguments. Use: " << argv[0] << " network <server/client> <ip_address> <network_port>" << std::endl;
 
-        delete communication;
-        communication = nullptr;
+        try
+        {
+            int com_type = Com_Type::NETWORK;
+            char* server_or_client = argv[2];
+            char* ip_address = argv[3];
+            if (!is_valid_ip(ip_address))
+            {
+                std::cerr << "Invalid IP address" << std::endl;
+                return 1;
+            }
+            int network_port = std::stoi(argv[4]);
+            unsigned short us_network_port = static_cast<unsigned short>(network_port);
+
+            Communication* communication = CommunicationObjectFactory::getObject(com_type, server_or_client, ip_address, us_network_port);
+            communication->handleMessage();
+
+            delete communication;
+            communication = nullptr;
+        }
+        catch (const std::invalid_argument& e)
+        {
+            std::cerr << "Invalid argument: " << e.what() << std::endl;
+            return 1;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return 1;
+        }
     }
-    catch (const std::invalid_argument &e)
+    else
     {
-        std::cerr << "Invalid argument: " << e.what() << std::endl;
-        return 1;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+        std::cout << "Please provide valid communication type" << std::endl;
     }
     return 0;
 }
