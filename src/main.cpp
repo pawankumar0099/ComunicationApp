ae1d285be24ee0d2d1536e36c2411728ccad67cb
pawan <pawan@nagarro.com>
2023-04-03 17:33:01 +0530

    some updates

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,17 @@
 int main(int argc, char *argv[])
 {
     // user did't provide any cmdline arguments
-    if (argc < 2)
+    if (argc == 1)
     {
-        std::cerr << "Invalid number of arguments. Use: " << argv[0] << " network <server/client> <ip_address> <network_port>" << std::endl;
+        std::cout << "Opstions :" << std::endl
+                  << "\t network" << std::endl
+                  << "\t\t server \t start application as a server" << std::endl
+                  << "\t\t client \t start application as a client" << std::endl
+                  << "\t\t ip address \t provide ip address" << std::endl
+                  << "\t\t port \t\t provide port of application" << std::endl
+                  << std::endl;
+
+        std::cerr << "Use: " << argv[0] << " network <server/client> <ip_address> <network_port>" << std::endl;
         return 1;
     }
 
@@ -19,7 +27,6 @@ int main(int argc, char *argv[])
         communication->handleMessage();
 
         delete communication;
-        
     }
     catch (const std::exception &e)
     {
